2a040b4ac40dad580447d24f5ebebc17adc47efe
joshe1129 <franshesco_save@outlook.com>
2015-09-10 09:20:10 -0500

    buclefor.cpp

---
+++ buclefor.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int contador=0;
+    for( contador=1; contador<11 ; contador++ ) {
+        printf("\n%d",contador);
+    };
+    return 0;
+}
